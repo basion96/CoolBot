52da0b0fd50fb7c8f82834baab42d953e9429a0f
DrTardis1 <techbryce@hotmail.com>
2020-04-26 21:05:55 +1000

    Added "!fuckyou" command, with text and image reply

--- src/commands/PingPongCommand.java
+++ src/commands/PingPongCommand.java
@@ -1,14 +1,19 @@
 package commands;
 
-
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import java.io.File;
 
 public class PingPongCommand extends ListenerAdapter {
 
     public void onMessageReceived(MessageReceivedEvent event){
         if(event.getMessage().getContentDisplay().equals("!ping")){
             event.getChannel().sendMessage("Pong!").queue();
+        }
+        else if(event.getMessage().getContentDisplay().equals("!fuckyou")){
+            event.getChannel().sendMessage("NO, FUCK YOU").queue();
+            event.getChannel().sendFile(new File("resources/images/middle-finger.jpg")).queue();
         }
     }
 }
